408c7d9ed1b027c9494b53b996081c1d270f33ea
JebManMan <joshua.muunoja@gmail.com>
2024-08-30 13:28:00 +1000

    Version 8 (Broken)

--- FreeCell_ASS3_Joshua.M/main.js
+++ FreeCell_ASS3_Joshua.M/main.js
@@ -1,30 +1,34 @@
-
-window.onload = window.onresize = function() {
-    var canvas = document.getElementById('canvas');
-    canvas.width = window.innerWidth * 0.75;
-    canvas.height = window.innerHeight * 0.8;
-}
-
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-
+const canvas = document.getElementById("canvas");
 
 class FreeCellGame 
 {
-    constructor(numberOfTableaus = 8, numberOfFreeCells = 4)
+    constructor(canvasInput, numberOfTableaus = 8, numberOfFreeCells = 4)
     {
         this.deck = new Deck();
-        this.canvas = document.getElementById("canvas");
+        this.canvas = canvasInput;
         this.ctx = this.canvas.getContext("2d");
         this.tableaus = [];
         this.freeCells = [];
         this.foundations = [];
         this.generateCardComponents(numberOfTableaus, numberOfFreeCells);
-        
+
+        this.updateCanvasSizeStart();
+
+        requestAnimationFrame(this.mainGameLoop.bind(this));
 
         //Begin the game
         //window.setInterval(this.mainGameLoop, 100);
-        this.mainGameLoop()
+        //this.mainGameLoop();
+    }
+
+    updateCanvasSizeStart()
+    {
+        window.onload = window.onresize = function()   
+        {
+        this.generatePositions(0.4);
+        this.canvas.width = window.innerWidth * 0.75;
+        this.canvas.height = window.innerHeight * 0.8;
+        }
     }
 
     geneateFoundations()
@@ -83,9 +87,34 @@ class FreeCellGame
         this.renderObjects(renderableObjects);
     }
 
-    generatePositions()
-    {
-        let x = 0;
+    generatePositions(tableauHeight)
+    {   
+
+        //Creating positions for tableaus and cards within
+        for(let i = 0; i < this.tableaus.length; i++)
+        {
+            //Values deciding offset 
+            let tableauOffset =  0.05 * this.canvas.width;
+            let emptySpace = 0.1 * this.canvas.width;
+
+            //
+            this.tableaus[i].x = tableauOffset + (this.canvas.width - emptySpace / this.tableaus.length) * i;
+            this.tableaus[i].y = this.canvas.height * tableauHeight;
+            
+            for (let j = 0; j < this.tableaus[i].cards.length; j++)
+            {
+                let card = this.tableaus[i].cards[j];
+                card.x = this.tableaus[i].x;
+                card.width = this.canvas.width * 0.05;
+                card.height = card.width * 1.4;
+                
+                card.y = this.tableaus[i].y + card.height * 0.4;
+                
+                this.tableaus[i].cards[j] = card;
+
+            }
+            console.log(this.tableaus)
+        }
     }
 
     renderObjects(renderableObjects)
@@ -93,19 +122,25 @@ class FreeCellGame
         for(let card of renderableObjects)
         {
             console.log(card);
-            card.x = 100;
-            card.y = 200;
-            let cardElement = new Image(); //document.createElement("img");
-            cardElement.src = card.getImageRef();
+            card.x = 20;
+            card.y = 20;
+            //var cardElement = new Image(); 
+            var cardElement = document.createElement("img");
+            cardElement.src = "cards/" + card.getImageRef();
 
-            this.ctx.drawImage(cardElement,card.x, card.y);
+            this.ctx.beginPath();
+            this.ctx.rect(20, 20, 150, 100);
+            this.ctx.stroke();
+            this.ctx.drawImage(cardElement,card.x, card.y, card.width, card.height);
         }
     }
 
     mainGameLoop()
     {
         this.updateDisplay();
-    
+        setTimeout(() => {
+            requestAnimationFrame(this.mainGameLoop.bind(this));
+        }, 100 );
         //update display 
         //generate hitBoxes
         //collect input
@@ -113,5 +148,5 @@ class FreeCellGame
         //After two clicks atempt to perform movement
     }
 }
+mainGameInstance = new FreeCellGame(canvas, 8,4);
 
-mainGameInstance = new FreeCellGame(8,4);
